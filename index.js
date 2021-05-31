3ad81f2df3a3241f79d0a668c4e95c65687f3e2a
DemaNadir <>
2021-05-31 17:51:16 +0000

    added css file

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
-"use strict";
+//"use strict";
 // console.log("dema");
 
-var userName = "dema";
- alert("dema")
+//var userName = "dema";
+ //alert("dema")
 
-document.write("<p>hi dema</p>"
-)
-var myFamilyNames = ['nadir', 'majdoleen', 'tamara', 'saed', 'noor',];
-console.log(myFamilyNames);
+//document.write("<p>hi dema</p>"
+//)
+//var myFamilyNames = ['nadir', 'majdoleen', 'tamara', 'saed', 'noor',];
+//console.log(myFamilyNames);
 
-if(userName==="dema"){console.log("hi dema")
-}else{console.log("hi")}
+//if(userName==="dema"){console.log("hi dema")
+//}//else{console.log("hi")}
 
-console.log(myFamilyNames[2]);
+//console.log(myFamilyNames[2]);
 
-console.log(myFamilyNames.length);
-function images(){
-for(var i = 2; i < myFamilyNames.length; i++){
-  console.log(myFamilyNames);
-}
+//console.log(myFamilyNames.length);
+//function images(){
+//for(var i = 2; i < myFamilyNames.length; i++){
+  //console.log(myFamilyNames);
+//}
 
-var sistername = prompt('plese enter name');
-var brothername = prompt('please enter the name');
+//var sistername = prompt('plese enter name');
+//var brothername = prompt('please enter the name');
 
-if(sistername == 'tamara' && brothername == 'saed'){
-  console.log(true);
-}
-for(var i = 2; i < myFamilyNames.length; i++) {
+//if(sistername == 'tamara' && brothername == 'saed'){
+  //console.log(true);
+//}
+//for(var i = 2; i < myFamilyNames.length; i++) {
 
-  document.write('<img src="https://www.mcicon.com/wp-content/uploads/2021/03/Cat-03.jpg" />')
+  //document.write('<img src="https://www.mcicon.com/wp-content/uploads/2021/03/Cat-03.jpg" />')
     
-  }
+  //}
 
-}
+//}
 
-images();
-
-
+//images();
 
 
-let num1 = 11;
-let num2 = 12;
-let num3 = 13;
-let num4 = 14;
-let num5 = 15;
 
-let total = num1 + num2 + num3 + num4 + num5;
-console.log(total);
 
-function sumTotal(n1, n2, n3, n4, n5){
-  let total1 = n1 + n2 + n3 + n4 + n5;
-  console.log(total);
-}
+//let num1 = 11;
+//let num2 = 12;
+//let num3 = 13;
+//let num4 = 14;
+//let num5 = 15;
+
+//let total = num1 + num2 + num3 + num4 + num5;
+//console.log(total);
+
+//function sumTotal(n1, n2, n3, n4, n5){
+  //let total1 = n1 + n2 + n3 + n4 + n5;
+  //console.log(total);
+//}
 
 
 
--- javascript.js
+++ javascript.js
@@ -2,33 +2,32 @@
 "use strict";
 // console.log("dema");
 
-var userName = "dema";
- alert("dema")
+//var userName = "dema";
+ //alert("dema")
 
-document.write("<p>hi dema</p>"
+//document.write("<p>hi dema</p>"
 )
-var myFamilyNames = ['nadir', 'majdoleen', 'tamara', 'saed', 'noor',];
-console.log(myFamilyNames);
+//var myFamilyNames = ['nadir', 'majdoleen', 'tamara', 'saed', 'noor',];
+//console.log(myFamilyNames);
 
-if(userName==="dema"){console.log("hi dema")
-}else{console.log("hi")}
+//if(userName==="dema"){console.log("hi dema")
+}//else{console.log("hi")}
 
-console.log(myFamilyNames[2]);
+//console.log(myFamilyNames[2]);
+//console.log(myFamilyNames.length);
 
-console.log(myFamilyNames.length);
-
-for(var i = 2; i < myFamilyNames.length; i++){
-  console.log(myFamilyNames);
+//for(var i = 2; i < myFamilyNames.length; i++){
+  //console.log(myFamilyNames);
 }
 
-var sistername = prompt('plese enter name');
-var brothername = prompt('please enter the name');
+//var sistername = prompt('plese enter name');
+//var brothername = prompt('please enter the name');
 
-if(sistername == 'tamara' && brothername == 'saed'){
-  console.log(true);
+//if(sistername == 'tamara' && brothername == 'saed'){
+  //console.log(true);
 }
-for(var i = 0; i < myFamilyNames.length; i++) {
+//for(var i = 0; i < myFamilyNames.length; i++) {
 
-  document.write('<img src="https://www.mcicon.com/wp-content/uploads/2021/03/Cat-03.jpg" />')
+  //document.write('<img src="https://www.mcicon.com/wp-content/uploads/2021/03/Cat-03.jpg" />')
     
   }
